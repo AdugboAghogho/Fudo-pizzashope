ae5074ac7296d4b37f54157b7329135efa214f8d
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-12 19:30:06 +0100

    footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import css from '../styles/Header.module.css'
 import Image from 'next/image'
 import Logo from '../assets/Logo.png'
+import { UilShoppingBag } from '@iconscout/react-unicons'
 
 const Header = () => {
   return (
@@ -14,6 +15,19 @@ const Header = () => {
           height={50}
         />
         <span>Fudo</span>
+      </div>
+
+      <ul className={css.menu}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className={css.rightSide}>
+        <div className={css.cart}>
+            <UilShoppingBag size={35} color='#2E2E2E' />
+            <div className={css.badge}>1</div>
+        </div>
       </div>
     </div>
   )
