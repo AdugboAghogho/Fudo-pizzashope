6c5841044a4b8d69b552e8cfe1628dc6d7dedadd
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-12 12:05:35 +0100

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,23 @@
-export default () => {
+import React from 'react'
+import css from '../styles/Header.module.css'
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+
+const Header = () => {
   return (
-     <div>
-        header
-     </div>
+    <div className={css.header}>
+      <div className={css.logo}>
+        <Image 
+          src={Logo}
+          alt='logo'
+          width={50}
+          height={50}
+        />
+        <span>Fudo</span>
+      </div>
+    </div>
   )
 }
+
+export default Header
+
