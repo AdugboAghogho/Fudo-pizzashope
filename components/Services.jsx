c353f8643e500dd83fdada8349e717433f63632a
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-14 10:34:00 +0100

    servies

---
+++ components/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import css from '../styles/Services.module.css'
+
+const Services = () => {
+  return (
+    <>
+      <div className={css.heading}>
+        <span>What WE Serve</span>
+        <span>Your Favourite Foof</span>
+        <span>Delivery Partner</span>
+      </div>
+    </>
+  )
+}
+
+export default Services
