5ddde9bbefc94010b172ad9d9f7726fecc3b6176
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-14 10:48:54 +0100

    sth 1

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import css from '../styles/Services.module.css'
+import s1 from '../assets/s1.png'
+import s2 from '../assets/s2.png'
+import s3 from '../assets/s3.png'
+import Image from 'next/image'
 
 const Services = () => {
   return (
@@ -8,6 +12,50 @@ const Services = () => {
         <span>WHAT WE SERVE</span>
         <span>Your Favourite Foof</span>
         <span>Delivery Partner</span>
+      </div>
+
+      <div className={css.services}>
+        <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+                <Image
+                  src={s1}
+                  alt=''
+                  objectFit='cover'
+                  layout='intrinsic'
+                />
+            </div>
+
+            <span>Easy to Order</span>
+            <span>You only need a few Steps in Food Ordering</span>
+        </div>
+
+        <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+                <Image
+                  src={s2}
+                  alt=''
+                  objectFit='cover'
+                  layout='intrinsic'
+                />
+            </div>
+
+            <span>Easy to Order</span>
+            <span>You only need a few Steps in Food Ordering</span>
+        </div>
+
+        <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+                <Image
+                  src={s3}
+                  alt=''
+                  objectFit='cover'
+                  layout='intrinsic'
+                />
+            </div>
+
+            <span>Easy to Order</span>
+            <span>You only need a few Steps in Food Ordering</span>
+        </div>
       </div>
     </>
   )
