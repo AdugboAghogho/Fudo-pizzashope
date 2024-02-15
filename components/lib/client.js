7b2ba654628e562263db1f28c67012c8a14c432c
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-15 13:54:14 +0100

    client .js

--- components/lib/client.js
+++ components/lib/client.js
@@ -1,7 +1,7 @@
-import sanityClient from "@sanity/client"
+import SanityClient from "@sanity/client"
 import { ImageUrlBuilder } from "@sanity/image-url";
 
-export const client = sanityClient ({
+export const client = SanityClient ({
     projectId: '2umrtm2g',
     dataset: 'production',
     apiVersion: '2024-02-16',
