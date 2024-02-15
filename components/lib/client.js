0fde14a93293b9ef35a465c5201ce8f2e09940c1
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-15 11:38:10 +0100

    ImageUrlBuilde

--- components/lib/client.js
+++ components/lib/client.js
@@ -1,4 +1,5 @@
 import sanityClient from "@sanity/client"
+import ImageUrlBuilder from "@sanity/image-url/lib/types/builder"
 
 export const client = sanityClient ({
     projectId: '2umrtm2g',
@@ -7,3 +8,7 @@ export const client = sanityClient ({
     useCdn: true,
     token: 'skHBeayrOaRAhpawcsaY01cKqnBdHiKeCHULQ8fR8lih9tVkqjbcHSQiZ1fgt8HGIzzcvw2FsdltqvM8OvE7aVNhLMjawCKFy0bxBD1a2juC4t2MPbjU7MTu3cCwJbs5uTlr2vFpJ621VSQZEopdC1mitbD7MtG12mDpSuJ7kTKDRC7uELsN'
 })
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source)
