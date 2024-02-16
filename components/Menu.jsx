38736d2bd63591c1b9673a215741f15394293e9b
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-16 21:11:53 +0100

    ImageWra

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import css from '../styles/Menu.module.css'
+import Image from 'next/image'
+import { urlFor } from './lib/client'
 
 const Menu = ({ pizzas }) => {
   return (
@@ -9,6 +11,24 @@ const Menu = ({ pizzas }) => {
         <span>Menu That Always</span>
         <span>Makes You Fall In Love</span>
       </div>
+
+      {pizzas.map((pizza, id) =>  {
+            const src = urlFor(pizza.image).url()
+
+            return (
+                <div className={css.pizza} key={id}>
+                    <div className={css.ImageWrapper}>
+                        <Image 
+                        loader={() => src}
+                        src={src}
+                        alt='pizza'
+                        objectFit='cover'
+                        layout='fill'
+                        />
+                    </div>
+                </div>
+            )
+       })}
     </div>
   )
 }
