4bfa97fd82da4181c4225874fb4b355551af0805
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-17 23:13:39 +0100

    brio

--- pages/pizza/[slug].js
+++ pages/pizza/[slug].js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Layout from './../../components/Layout';
-import { client } from '../../components/lib/client';
+import { client, urlFor } from '../../components/lib/client';
+import Image from 'next/image';
+import css from '../../styles/Pizza.module.css'
 
 export default function pizza ({ pizza }) {
+  const src = urlFor(pizza.image).url()
+
   return (
     <Layout>
-      slug
+      <div className={css.container}>
+        <div className={css.imageWrapper}>
+            <Image
+              loader={() => src}
+              alt=''
+              src={src}
+              layout='fill'
+              unoptimized
+              objectFit='cover '
+            />
+        </div>
+      </div>
     </Layout>
   )
 }
@@ -19,4 +34,18 @@ export async function getStaticPaths() {
         paths: paths.map((slug) => ({params: {slug}})),
         fallback: 'blocking'
     }
+}
+
+
+export async function getStaticPaths(context){
+    const { slug = ""} = context.params;
+    const pizza = await client.fetch(
+        `*[_type=="pizza" && slug.current == ${slug}][0]`
+    );
+
+    return {
+        props: {
+            pizza,
+        },
+    };
 }
