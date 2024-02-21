2545addc58594d9be48f42a95b01a48bbe08feff
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-21 17:03:05 +0100

    store1

--- store/store.js
+++ store/store.js
@@ -1,26 +1,25 @@
 import create from 'zustand';
-import Cart from './../pages/cart';
 
 export const useStore = create (
     (set) => ({
         // CART
-        Cart : {
+        cart : {
             pizzas : []
         },
 
         // ADD PIZZA IN CART
         addPizza : (data) => 
         set ((state) => ({
-            Cart: {
-                pizzas: [...state.Cart.pizzas, data]
+            cart: {
+                pizzas: [...state.cart.pizzas, data]
             }
         })),
 
         // REMOVE PIZZA
         removePizza : (index) => 
         set((state) => ({
-            Cart : {
-                pizzas : state.Cart.pizzas.filter((_, i) => i !=index)
+            cart : {
+                pizzas : state.cart.pizzas.filter((_, i) => i !=index)
             }
         }))
     })
