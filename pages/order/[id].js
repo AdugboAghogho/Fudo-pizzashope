de56ab0d6241962fa37abfb97e4c00a2191842c7
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-24 14:42:34 +0100

    statusConatiner

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -17,11 +17,49 @@ export default function ({order}) {
 
     return(
         <Layout>
-          <div className={css.container}>
-             <span className={css.heading}>
-               Order in process
-             </span>
-          </div>
+           <div className={css.container}>
+               <span className={css.heading}>
+                   Order in process
+                </span>
+               <div className={css.details}>
+                    <div>
+                        <span>Order ID</span>
+                        <span>{order._id}</span>
+                    </div>
+                    <div>
+                        <span>Customer Name</span>
+                        <span>{order.name}</span>
+                    </div>
+                    <div>
+                        <span>Phone</span>
+                        <span>{order.phone}</span>
+                    </div>
+                    <div>
+                        <span>Method</span>
+                        <span>
+                            {
+                                order.method === 0 ? 'Cash On Delivery' : 'Online Payment(Paid)'
+                            }
+                        </span>
+                    </div>
+                    <div>
+                        <span>Total</span>
+                        <span>$ {order.total}</span>
+                    </div>
+               </div>
+            
+
+            <div className={css.statusConatiner}>
+                <div className={css.status}>
+                    <UilBill width={50} height={50}/>
+                    <span>Payment</span>
+                    {order.method === 0 ?
+                       <span className={css.pending}>On Delivery</span> :
+                       <span className={css.pending}>Completed</span> 
+                    }
+                </div> 
+            </div>
+            </div>
         </Layout>
     )
 }
