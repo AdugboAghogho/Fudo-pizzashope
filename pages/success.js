acb261029ac0698056936451bb64e90713461f67
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-27 12:06:43 +0100

    ordermodel

--- pages/success.js
+++ pages/success.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import OrderModal from '../components/OrderModal'
 
 const success = () => {
   return (
     <Layout>
-      .
+      <OrderModal opened={true} PaymentMethod={1} />
     </Layout>
   )
 }
